0541801defc049408a1aade8e877935f81307f0b
a22405793 <vini.vivoco@icloud.com>
2025-12-13 22:44:24 +0000

    lkjhgf

--- src/pt/ulusofona/lp2/greatprogrammingjourney/ErroDeLogica.java
+++ src/pt/ulusofona/lp2/greatprogrammingjourney/ErroDeLogica.java
@@ -14,27 +14,21 @@ public class ErroDeLogica extends Abismos {
     @Override
     public String aplicarEfeito(Player jogador, Board board, int valorDado) {
 
-        int posAtual = jogador.getPosicao();
-        int n = valorDado / 2;
+        int recuo = valorDado / 2; // floor automaticamente
 
-        // Se por algum motivo valorDado vier 0 ou negativo, não recua
-        if (n <= 0) {
+        if (recuo <= 0) {
             return "O programador " + jogador.getNome()
                     + " caiu num Erro de Lógica, mas o impacto foi nulo e não recuou casas.";
         }
-        int novaPos = posAtual - n;
-        if (novaPos < 1) {
-            novaPos = 1; // nunca abaixo da casa 1
-        }
-        jogador.setPosicao(novaPos, board.getSize());
-        if (novaPos < posAtual) {
-            return "O programador " + jogador.getNome()
-                    + " cometeu um Erro de Lógica e recuou " + n
-                    + " casas, indo para a posição " + novaPos + ".";
-        } else {
-            // Caso extremo em que já estava na 1 e não pode recuar
-            return "O programador " + jogador.getNome()
-                    + " cometeu um Erro de Lógica, mas já estava na primeira casa e não recuou.";
-        }
+
+        // mover no TABULEIRO
+        board.movePlayer(jogador.getId(), -recuo);
+
+        int novaPos = board.getPlayerPosicao(jogador.getId());
+
+        return "O programador " + jogador.getNome()
+                + " cometeu um Erro de Lógica e recuou " + recuo
+                + " casas, indo para a posição " + novaPos + ".";
     }
+
 }
